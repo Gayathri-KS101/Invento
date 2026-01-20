ad8d1e9a54567750eb597706d97825327c2fbd70
Muflih-uk <muhammedmuflih875@gmail.com>
2026-01-20 21:12:26 +0530

    feat: create Preview Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import HeroSection from "../components/pages/home/HeroSection";
 import FAQSection from "../components/pages/home/FAQSection";
 import ContactSection from "../components/pages/home/ContactSection";
+import Preview from "../components/pages/home/preview/Preview";
+import ProShow from "../components/pages/home/preview/ProShow";
 
 
 
 export default function HomePage() {
     return(
-        <div>
+        <div className="flex flex-col">
             <HeroSection/>
             <FAQSection/>
             <ContactSection/>
+            <ProShow/>
+            <Preview/>
         </div>       
     );
 }
